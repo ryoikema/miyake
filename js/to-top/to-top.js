bdd0695a71a980eb96268d968e3d1cac66b84160
Ryo Ikema <ryoikema@gmail.com>
2017-04-07 11:25:28 +0900

    追加されに

---
+++ js/to-top/to-top.js
@@ -0,0 +1,18 @@
+
+$(function() {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+        return false;
+    });
+});
